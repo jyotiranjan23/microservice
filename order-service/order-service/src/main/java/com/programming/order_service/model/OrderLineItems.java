da8f58113f366274a9af000457bb3321bf7ee504
jpanda <pandajyoti.2501@gmail.com>
2025-01-17 12:35:50 +0530

    Pushing the code

---
+++ order-service/order-service/src/main/java/com/programming/order_service/model/OrderLineItems.java
@@ -0,0 +1,27 @@
+package com.programming.order_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "t_order_line_items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private double price;
+    private int quantity;
+
+}
